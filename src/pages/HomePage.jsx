7e8368ead47a1ea871c7c299450bb158eca44f12
zakizaidi55 <zakizaidi55@gmail.com>
2024-05-05 22:24:40 +0530

    useCall back from react

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 
 function HomePage() {
     const [value, setValue] = useState('');
